40bc36591ba2aeb856e7e929cfc70b38707a03b2
speezy <93849730+notspeezy@users.noreply.github.com>
2022-07-17 08:53:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from platform import release
 import requests, httpx, time, sys, os; from itertools import cycle; from colorama import Fore; from concurrent.futures import ThreadPoolExecutor
     
 os.system("clear") if sys.platform == "linux" else os.system("cls")
@@ -21,10 +20,24 @@ class Cleaner:
         self.cleaned = []
         
     
-    def getcookies(self):
-        response = requests.get("https://discord.com/api/v9/experiments")
-        cookie = response.cookies.get_dict()
-        return cookie
+    def get_cookies(self):
+    headerz = {
+        "Host": "discord.com",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Cache-Control": "max-age=0",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Upgrade-Insecure-Requests": "1"
+    }
+    response = requests.get("https://discord.com", headers=headerz)
+    cookie = response.cookies.get_dict()
+    return cookie
         
     
     def guildcleaner(self, token):
